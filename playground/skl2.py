14c14c634fdbe85aa52314fe6a24f264703ff8ff
Phil <philipg7@vt.edu>
2017-10-19 14:39:18 -0400

    sgdRegressor

--- playground/skl2.py
+++ playground/skl2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import json
 from sklearn.neural_network import MLPRegressor
+from sklearn.linear_model import SGDRegressor
 import random 
 import sys
 import pickle
@@ -34,11 +35,18 @@ for filename in sys.argv[1:]:
 print("Done tickling the pickle.")
 
 print("Creating the neural network. Starting training...")
-clf = MLPRegressor(hidden_layer_sizes=(250,150,40),  activation='relu', solver='adam', alpha=0.001,batch_size='auto',
-               learning_rate='adaptive', learning_rate_init=0.01, power_t=0.5, max_iter=100000, shuffle=True,
-               random_state=None, tol=0.0000001, verbose=True, warm_start=False, momentum=0.9,
-               nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999,
-               epsilon=1e-08)
+#clf = MLPRegressor(hidden_layer_sizes=(250,150,40),  activation='relu', solver='adam', alpha=0.001,batch_size='auto',
+#               learning_rate='adaptive', learning_rate_init=0.01, power_t=0.5, max_iter=100000, shuffle=True,
+#               random_state=None, tol=0.0000001, verbose=True, warm_start=False, momentum=0.9,
+#               nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999,
+#               epsilon=1e-08)
+
+clf = SGDRegressor(alpha=0.0001, average=False, epsilon=0.1, eta0=0.01,
+       fit_intercept=True, l1_ratio=0.15, learning_rate='invscaling',
+       loss='squared_loss', max_iter=5, n_iter=None, penalty='l2',
+       power_t=0.25, random_state=None, shuffle=True, tol=None,
+       verbose=0, warm_start=False)
+
     
 clf.fit(inputs, outputs)
 from sklearn.externals import joblib

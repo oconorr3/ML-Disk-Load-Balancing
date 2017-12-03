fb06bc3e925bb04cde97ef7bfa4c5170b40eefc2
Robert Schofield <rjschof@vt.edu>
2017-12-03 18:31:47 -0500

    Pushing config.

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ num_input_lines = 5
 # The scaler to use for normalization.
 scaler = QuantileTransformer(output_distribution='normal')
 
-sgd_maxiter = 50
+sgd_maxiter = 3000
 n_jobs = 2
 
 ###########################################################
@@ -43,33 +43,33 @@ sklmethods = {
     # Set up MLPClassifier
     ######################################################
     # MLPClassifier with relu activation function:
-    'mlp-relu1': MLPClassifier(hidden_layer_sizes=(1500), alpha=0.001, batch_size=2000,
-            learning_rate='adaptive', power_t=0.5, max_iter=2000, shuffle=True, random_state=None,
-            tol=0.000001, warm_start=False, momentum=0.9, nesterovs_momentum=True,
-            early_stopping=True, validation_fraction=0.90, beta_1=0.9, beta_2=0.999, epsilon=1e-08,
-            activation='relu', solver='adam', learning_rate_init=0.75, verbose=True),
-    'mlp-relu2': MLPClassifier(hidden_layer_sizes=(1500), alpha=0.001, batch_size=2000,
+    'mlp-relu1': MLPClassifier(hidden_layer_sizes=(350), alpha=0.001, batch_size=100000,
+            learning_rate='adaptive', power_t=0.5, max_iter=2000, shuffle=False, random_state=None,
+            tol=0.000001, warm_start=True, momentum=0.7, nesterovs_momentum=False,
+            early_stopping=False, validation_fraction=0.9, beta_1=0.9, beta_2=0.999, epsilon=1e-08,
+            activation='relu', solver='sgd', learning_rate_init=0.95, verbose=True),
+    'mlp-relu2': MLPClassifier(hidden_layer_sizes=(100), alpha=0.001, batch_size=2000,
             learning_rate='adaptive', power_t=0.5, max_iter=2000, shuffle=True, random_state=None,
             tol=0.000001, warm_start=False, momentum=0.9, nesterovs_momentum=True,
             early_stopping=True, validation_fraction=0.90, beta_1=0.9, beta_2=0.999, epsilon=1e-08,
             activation='relu', solver='sgd', learning_rate_init=0.75, verbose=True),
-    'mlp-relu2': MLPClassifier(hidden_layer_sizes=(1500), alpha=0.001, batch_size=2000,
+    'mlp-relu3': MLPClassifier(hidden_layer_sizes=(100), alpha=0.001, batch_size=2000,
             learning_rate='adaptive', power_t=0.5, max_iter=2000, shuffle=True, random_state=None,
             tol=0.000001, warm_start=False, momentum=0.9, nesterovs_momentum=True,
             early_stopping=True, validation_fraction=0.90, beta_1=0.9, beta_2=0.999, epsilon=1e-08,
             activation='relu', solver='lbfgs', learning_rate_init=0.75, verbose=True),
     # MLPClassifier with logistic activation function:
-    'mlp-logistic1': MLPClassifier(hidden_layer_sizes=(1500), alpha=0.001, batch_size=2000,
+    'mlp-logistic1': MLPClassifier(hidden_layer_sizes=(100), alpha=0.001, batch_size=2000,
             learning_rate='adaptive', power_t=0.5, max_iter=2000, shuffle=True, random_state=None,
             tol=0.000001, warm_start=False, momentum=0.9, nesterovs_momentum=True,
             early_stopping=True, validation_fraction=0.90, beta_1=0.9, beta_2=0.999, epsilon=1e-08,
             activation='logistic', solver='adam', learning_rate_init=0.75, verbose=True),
-    'mlp-logistic2': MLPClassifier(hidden_layer_sizes=(1500), alpha=0.001, batch_size=2000,
+    'mlp-logistic2': MLPClassifier(hidden_layer_sizes=(100), alpha=0.001, batch_size=2000,
             learning_rate='adaptive', power_t=0.5, max_iter=2000, shuffle=True, random_state=None,
             tol=0.000001, warm_start=False, momentum=0.9, nesterovs_momentum=True,
             early_stopping=True, validation_fraction=0.90, beta_1=0.9, beta_2=0.999, epsilon=1e-08,
             activation='logistic', solver='sgd', learning_rate_init=0.75, verbose=True),
-    'mlp-logistic3': MLPClassifier(hidden_layer_sizes=(1500), alpha=0.001, batch_size=2000,
+    'mlp-logistic3': MLPClassifier(hidden_layer_sizes=(100), alpha=0.001, batch_size=2000,
             learning_rate='adaptive', power_t=0.5, max_iter=2000, shuffle=True, random_state=None,
             tol=0.000001, warm_start=False, momentum=0.9, nesterovs_momentum=True,
             early_stopping=True, validation_fraction=0.90, beta_1=0.9, beta_2=0.999, epsilon=1e-08,
@@ -91,8 +91,8 @@ sklmethods = {
             random_state=None, shuffle=True, tol=None, warm_start=False,
             verbose=True, loss='huber', penalty='elasticnet', max_iter=sgd_maxiter, n_jobs=n_jobs),
     'sgd-modhuber': SGDClassifier(alpha=0.0001, average=False, epsilon=0.05, eta0=0.0025,
-            fit_intercept=False, l1_ratio=0.10, learning_rate='optimal', power_t=0.05,
-            random_state=None, shuffle=True, tol=None, warm_start=False,
+            fit_intercept=False, l1_ratio=0.10, learning_rate='invscaling', power_t=0.05,
+            random_state=None, shuffle=False, tol=None, warm_start=False,
             verbose=True, loss='modified_huber', penalty='l2', max_iter=sgd_maxiter, n_jobs=-1),
     'sgd-perceptron': SGDClassifier(alpha=0.0001, average=False, epsilon=0.05, eta0=0.0025,
             fit_intercept=False, l1_ratio=0.10, learning_rate='optimal', power_t=0.05,
